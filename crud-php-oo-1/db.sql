a7a6ea12def8c92209d5a505f2750798585ae72a
Barbosaadev <silvag1212@gmail.com>
2022-11-30 11:37:38 -0300

    readme e createadmin

--- crud-php-oo-1/db.sql
+++ crud-php-oo-1/db.sql
@@ -17,11 +17,10 @@ CREATE TABLE tb_professores (
     id INT PRIMARY KEY AUTO_INCREMENT,
     endereco VARCHAR(45) NOT NULL,
     formacao VARCHAR(45) NOT NULL,
-    status VARCHAR(45) NOT NULL,
+    status TINYINT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     cpf CHAR(11) UNIQUE NOT NULL
 );
-
 
 CREATE TABLE tb_user (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -30,39 +29,18 @@ CREATE TABLE tb_user (
     password VARCHAR(255) NOT NULL,
     profile VARCHAR(20) NOT NULL
 );
-CREATE TABLE tb_curso (
+
+CREATE TABLE tb_categorias (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    periodo VARCHAR(50) NOT NULL,
-    professor VARCHAR(50) NOT NULL,
-    laboratorio VARCHAR(50) NOT NULL
+    nome VARCHAR(50) NOT NULL
 );
 
-
-
-INSERT INTO tb_alunos 
-(nome, matricula, email, status, genero, dataNascimento, cpf)
-VALUES
-('Maria', '1234123', 'maria@email.com', true, 'Feminino', '2001-09-12', '12312312312'),
-('Chiquim', '4434123', 'chiquim@email.com', true, 'Masculino', '2000-12-31', '44455588812'),
-('Joaquim', '5534123', 'joaquim@email.com', true, 'Não informado', '1997-06-27', '09812312390');
-
-SELECT * FROM tb_alunos;
-
-INSERT INTO tb_professores
-(endereco, formacao, status, nome, cpf)
-VALUES
-('Rua barca semi nova 123', 'HTML, CSS, JS, React', true, 'Alessandro', '12345612345'),
-('Rua idelfonso albano 222, ap 1403', 'SABE TUDO, BRABISSIMO', true,'Allan', '99999999999'),
-('Rua oscar frança 88', 'Formado nas ruas', true,'Gleidson', '22222222222');
-
-SELECT * FROM tb_professores;
-
-INSERT INTO tb_cursos
-(nome, periodo, professor, laboratorio)
-VALUES
-('php', 'manham', 'Alessandro', 'bill gates'),
-('css', 'tarde','Allan', 'steav jobs'),
-('javascript', 'noite','Gleidson', 'bill gates');
-
-SELECT * FROM tb_cursos;
+CREATE TABLE tb_cursos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    cargaHoraria VARCHAR(50) NOT NULL,
+    descricao VARCHAR(100) UNIQUE NOT NULL,
+    status TINYINT NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES tb_categorias(id)
+);
